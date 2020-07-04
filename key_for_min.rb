eed4cb94d120bd4e52bcbf73eb61e7961a90387a
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-04 13:40:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash.each do |name, age|
   lowest_age = 1000
   youngest = ""
+name_hash.each do |name, age|
   if age < lowest_age
     lowest_age = age 
     youngest = name 
