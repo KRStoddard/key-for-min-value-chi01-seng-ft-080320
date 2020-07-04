eb3de8e14d8c8abca448e8edba8378764d00b217
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-04 13:40:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,12 @@
 
 def key_for_min_value(name_hash)
 name_hash.each do |name, age|
-  lowest_age = 1000 
+  lowest_age = 1000
+  youngest = ""
   if age < lowest_age
-    
-  
+    lowest_age = age 
+    youngest = name 
+  end
+end 
+  youngest 
 end
